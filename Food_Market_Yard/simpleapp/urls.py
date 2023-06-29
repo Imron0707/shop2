399b593fddc0c99c946386ab52fd287845e1c2b3
Name <email@example.ru>
2023-06-29 21:17:09 +0600

    Initial commit

---
+++ Food_Market_Yard/simpleapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+# Импортируем созданное нами представление
+from .views import ProductsList
+from .views import ProductDetail
+
+
+urlpatterns = [
+   # path — означает путь.
+   # В данном случае путь ко всем товарам у нас останется пустым,
+   # чуть позже станет ясно почему.
+   # Т.к. наше объявленное представление является классом,
+   # а Django ожидает функцию, нам надо представить этот класс в виде view.
+   # Для этого вызываем метод as_view.
+   path('', ProductsList.as_view()),
+   path('<int:pk>', ProductDetail.as_view()),
+]
---
+++ Food_Market_Yard/simpleapp/views.py
@@ -0,0 +1,49 @@
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from .models import Product
+from datetime import datetime
+
+
+class ProductsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Product
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = 'name'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'products.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'products'
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = None
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = "Распродажа в среду!"
+        return context
+
+
+class ProductDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Product
+    # Используем другой шаблон — product.html
+    template_name = 'product.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'product'
